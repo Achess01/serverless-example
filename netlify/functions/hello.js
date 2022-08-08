70b16b9f873b51a74225054f95f6fa441dee1c2a
Achess <jealtzo26@gmail.com>
2022-08-08 01:33:24 -0600

    Adding number * 2

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,12 @@
 exports.handler = async function (event, context) {
+  let number = event.queryStringParameters.num || 0;
+  if (!isNaN(number)) {
+    number = 0;
+  } else {
+    number = parseInt(number);
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World", result: number * 2 }),
   };
 };
